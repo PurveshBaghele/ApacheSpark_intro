fde35972d3cffb970e05b2e23073379007ccc9a7
Pradyot <pradyotpatil@gmail.com>
2019-10-04 21:10:18 +0200

    added lab2

---
+++ lab1/src/main/scala/Group10_RDD.scala
@@ -0,0 +1,79 @@
+package group10
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions.split 
+import org.apache.log4j.{Level, Logger}
+import java.sql.Timestamp
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions._
+
+
+
+object Group10{
+	
+  def main(args: Array[String]) {
+
+	Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+	
+	val spark = SparkSession
+      .builder
+      .appName("Group10")
+      //.config("spark.master", "local")
+      .getOrCreate()
+	  
+	val sc = spark.sparkContext // If you need SparkContext object
+
+    import spark.implicits._
+    val sqlContext = new SQLContext(sc);
+    import sqlContext.implicits._
+
+val t2 = System.nanoTime          
+
+// val grdd=sc.textFile("./data/segment/") //read all the csv files
+val grdd=sc.textFile(args(0)) //read all the csv files
+
+val transform1=grdd.map(x=>x.split("\t")).filter(col=>col.size>23 && col(23)!="") //filter entries having allNames column null
+
+val rdd1= transform1.map(col=>(col(1).substring(0, 4)+"-"+col(1).substring(4, 6) +"-"+ col(1).substring(6, 8),col(23).split(";")))// map date and AllNames
+                          .mapValues(t=>t.map(s=>(s.split(",")(0),1)))// split allNames without distinct and map (topics with 1)
+                          .reduceByKey((x,y)=>x ++ y) //ReduceByKey on date
+                          .map(x => {(x._1, x._2.groupBy(_._1).mapValues(_.map(_._2).sum).toArray.sortBy(t=>t._2).reverse.take(10))}) //count topics then sort and reverse
+                          //.collect().foreach(col=>println(col._1,col._2.mkString(" "))) //print the RDD
+
+
+
+
+
+val rdd2= transform1.map(col=>(col(1).substring(0, 4)+"-"+col(1).substring(4, 6) +"-"+ col(1).substring(6, 8),col(23).split(";")))// map date and AllNames
+                          .mapValues(t=>t.map(s=>(s.split(",")(0),1)).distinct)// split allNames with distinct and map (topics with 1)
+                          .reduceByKey((x,y)=>x ++ y) //ReduceByKey on date
+                          .map(x => {(x._1, x._2.groupBy(_._1).mapValues(_.map(_._2).sum).toArray.sortBy(t=>t._2).reverse.take(10))}) //count topics then sort and reverse
+                          .collect().foreach(col=>println(col._1,col._2.mkString(" "))) //print the rdd
+
+
+
+val duration2 = (System.nanoTime - t2) / 1e9d
+println("rdd implementation execution time: "+duration2)
+
+spark.stop()
+    
+  }
+}
+
+ 
+
+  /*OUTPUT
+  // without Distinct
+  (2015-02-18,(Type ParentCategory,2290) (Islamic State,1787) (United States,1210) (New York,727) (White House,489) (Los Angeles,424) (Associated Press,385) (New Zealand,353) (United Kingdom,325) (Jeb Bush,298))
+  (2015-02-19,(United States,1497) (Islamic State,1233) (New York,1058) (United Kingdom,735) (White House,723) (Los Angeles,620) (New Zealand,590) (Associated Press,498) (San Francisco,479) (Practice Wrestling Room,420))
+  
+  //with Distinct
+  (2015-02-18,(United States,843) (New York,521) (Islamic State,459) (Associated Press,300) (Los Angeles,286) (White House,281) (United Kingdom,211) (Middle East,172) (New Zealand,170) (President Barack Obama,169))
+  (2015-02-19,(United States,1025) (New York,756) (Islamic State,461) (United Kingdom,425) (Associated Press,421) (White House,403) (Los Angeles,395) (New Zealand,297) (San Francisco,262) (President Barack Obama,253))
+
+*/
+
+
+
---
+++ lab2/src/main/scala/Group10_DF.scala
@@ -0,0 +1,145 @@
+package group10
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions.split 
+import org.apache.log4j.{Level, Logger}
+import java.sql.Timestamp
+import org.apache.spark.sql.functions._
+
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{rank, desc}
+
+object Group10_DF{
+	case class GData ( 
+				GKGRECORDID: String, 
+				Gdate: Timestamp, 
+				SourceCollectionIdentifier: Integer,
+				SourceCommonName: String,
+				DocumentIdentifier: String,
+				Counts: String,
+				V2Counts: String,
+				Themes: String,
+				V2Themes: String,
+				Locations: String,
+				V2Locations: String,
+				Persons: String,
+				V2Persons: String,
+				Organizations: String,
+				V2Organizations: String,
+				V2Tone: String,
+				Dates: String,
+				GCAM: String,
+				SharingImage: String,
+				RelatedImages: String,
+				SocialImageEmbeds: String,
+				SocialVideoEmbeds: String,
+				Quotations: String,
+				AllNames: String,
+				Amounts: String,
+				TranslationInfo: String,
+				Extras: String 
+			  )
+
+	def main(args : Array[String]){
+			
+        Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+			val schema = StructType( 
+					Array( StructField("GKGRECORDID", StringType), 
+						   StructField("Gdate", TimestampType), 
+						   StructField("SourceCollectionIdentifier", IntegerType), 
+						   StructField("SourceCommonName", StringType), 
+						   StructField("DocumentIdentifier", StringType), 
+						   StructField("Counts", StringType), 
+						   StructField("V2Counts", StringType), 
+						   StructField("Themes", StringType), 
+						   StructField("V2Themes", StringType), 
+						   StructField("Locations",StringType), 
+						   StructField("V2Locations",StringType), 
+						   StructField("Persons",StringType), 
+						   StructField("V2Persons",StringType), 
+						   StructField("Organizations",StringType), 
+						   StructField("V2Organizations",StringType), 
+						   StructField("V2Tone", StringType), 
+						   StructField("Dates",StringType), 
+						   StructField("GCAM", StringType), 
+						   StructField("SharingImage", StringType), 
+						   StructField("RelatedImages",StringType), 
+						   StructField("SocialImageEmbeds",StringType), 
+						   StructField("SocialVideoEmbeds",StringType), 
+						   StructField("Quotations", StringType),
+						   StructField("AllNames", StringType), 
+						   StructField("Amounts",StringType), 
+						   StructField("TranslationInfo",StringType), 
+						   StructField("Extras", StringType)
+					)
+				  )
+				  	  
+				val spark = SparkSession
+							.builder
+							.appName("Group10")
+							.config("spark.master", "local")
+							.getOrCreate()
+				val sc = spark.sparkContext
+				import spark.implicits._
+
+				val t2 = System.nanoTime
+				val ds = spark.read 
+							  .schema(schema)
+							  .option("timestampFormat", "yyyyMMddhhmmss") 
+							  .option("sep","\t")
+							  .csv("./data/segment/*.csv")
+							  .as[GData]
+							  
+
+				
+			val urlCleaner = (s:String) => {
+			   if (s == null) null else s.replaceAll(","," ").replaceAll("\\d+(?:[.,]\\d+)*\\s*", "")
+			}
+
+			val url_cleaner_udf = udf(urlCleaner)
+            
+			val filter2=ds.select("Gdate","AllNames")  //select Gdate and AllNames
+			val a = filter2.withColumn("AllNames",split(col("AllNames"),";").cast("array<String>")) //split using semicolon
+            val b = a.withColumn("AllNames",explode($"AllNames")) //explode AllNames
+            val c = b.withColumn("AllNames", url_cleaner_udf(b("AllNames"))) //using user defined function to remove the numbers
+            val newDS = c.withColumn("Gdate", c("Gdate").cast(DateType)) // cast timestamp to date
+            val counts = newDS.groupBy($"Gdate",$"AllNames").agg(count($"AllNames").as("count")) // count the topic occurence
+			
+            counts.withColumn("rank", rank().over(Window.partitionBy("Gdate").orderBy($"count".desc))) //descending order
+                .filter($"rank" <= 10) // select top 10
+                .drop("rank").withColumn("NewColumn", struct(counts("AllNames"), counts("count")))// combine topic and count
+                .select($"Gdate",$"NewColumn")
+                .groupBy($"Gdate").agg(collect_list($"NewColumn").as("AllNames"))
+                .rdd.map(_.toSeq.toList)
+                .foreach(println)
+			
+            val duration2 = (System.nanoTime - t2) / 1e9d
+			println("rdd implementation execution time: "+duration2)
+				
+				
+			/*
+				To save into a json file
+				
+				counts.withColumn("rank", rank().over(Window.partitionBy("Gdate").orderBy($"count".desc)))
+							.filter($"rank" <= 10)
+							.drop("rank")
+							.write
+							.mode("append")
+							.json("E:\\supercomputigLab\\SBD-tudelft\\lab1\\data\\segment\\report.json")
+			*/			
+			
+			
+		}
+	}
+ 
+
+/*OUTPUT
+List(2015-02-19, WrappedArray([United States ,1497], [Islamic State ,1233], [New York ,1058], [United Kingdom ,735], [White House ,723], [Los Angeles ,620], [New Zealand ,590], [Associated Press ,498], [San Francisco ,479], [Practice Wrestling Room ,420]))
+List(2015-02-18, WrappedArray([Type ParentCategory ,2290], [Islamic State ,1787], [United States ,1210], [New York ,727], [White House ,489], [Los Angeles ,424], [Associated Press ,385], [New Zealand ,353], [United Kingdom ,325], [Jeb Bush ,298]))
+rdd implementation execution time: 45.668898278
+*/
